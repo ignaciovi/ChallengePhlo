fd8d7dbcea183c32c1a7bb720206c2a0f96b849c
ignaciovi <ignvaldelvira@gmail.com>
2020-06-01 19:29:31 +0100

    Get doctors from data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app  = express();
+const {getDoctors} = require('./controllers/doctors')
 
+app.use('/getDoctors', (req, res) => res.send(getDoctors(req,res)))
 app.use('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
---
+++ backend/data/index.js
@@ -0,0 +1,8 @@
+const doctors = [
+  { id: 1, name: "Spitalfields Medical Centre - Health E1"},
+  { id: 2, name: "Whitechapel Health Centre Branch"}
+];
+
+module.exports = {
+  doctors
+}
