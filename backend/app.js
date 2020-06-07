82006883dad6aa623e51dc90ba29a2c531d009a7
ignaciovi <ignvaldelvira@gmail.com>
2020-06-07 22:31:48 +0100

    API improvements POST and GET

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,24 @@ const app  = express();
 const {getDoctors} = require('./controllers/doctors')
 const {sendEmail} = require('./mailer/Mailer')
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.use('/getDoctors', async (req, res) => {
+app.get('/getDoctors', async (req, res) => {
   output = await getDoctors(req)
-  return res.json(output)
+  if(output === []) {
+    return res.status(404).json(output)
+  } else {
+    return res.status(200).json(output)
+  }
 })
 
-app.use('/sendEmail', async (req, res) => {
+app.post('/sendEmail', async (req, res) => {
   output = await sendEmail(req)
   return res.json(output)
 })
-
 
 app.use('/health', (req, res) => res.send('Server running OK'))
 
